3fb6be7a1421335c87e185d7b81c9bd8377d69c7
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-05-13 12:00:11 -0300

    Capitulo_4_Exercicio_3

--- main.cpp
+++ main.cpp
@@ -7,16 +7,27 @@ using namespace std;
 
 int main()
 {
-    vector<double>numbers;
-    cout << "Enter numbers: " << '\n';
-    for(int i; cin>>i;)
-        numbers.push_back(i);
-    std::sort(numbers.begin(), numbers.end());
-    if(numbers.size()%2!=0)
-        cout << "\nThe median is: "
-             << numbers[(numbers.size()-1)/2];
-    else
-        cout << "\nThe median is: "
-             << (numbers[numbers.size()/2-1]+numbers[numbers.size()/2])/2
-             << '\n';
+   double sum = 0; //Total distance
+   double smallest = 0;
+   double greatest = 0;
+   double mean = 0;
+
+   vector<double>distances;
+   cout << "Enter distances between two cities: ";
+   for(double i; cin>>i;)
+      {
+          distances.push_back(i);
+          sum+=i; //Total distance
+      }
+      cout << "Total: " << sum << '\n';
+
+   std::sort(distances.begin(), distances.end());
+   smallest = distances[0];
+   greatest = distances[distances.size()-1];
+   mean = sum/distances.size();
+   cout << "Smallest: " << smallest
+        << "\nLargest: " << greatest
+        << "\nMean distance: " << mean
+        << '\n';
+
 }

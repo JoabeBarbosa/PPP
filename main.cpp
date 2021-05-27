693927cf5930602e97947fd1686026ced860d669
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-05-27 20:49:44 -0300

    Capitulo_4_Exercicio_4

--- main.cpp
+++ main.cpp
@@ -7,27 +7,28 @@ using namespace std;
 
 int main()
 {
-   double sum = 0; //Total distance
-   double smallest = 0;
-   double greatest = 0;
-   double mean = 0;
-
-   vector<double>distances;
-   cout << "Enter distances between two cities: ";
-   for(double i; cin>>i;)
-      {
-          distances.push_back(i);
-          sum+=i; //Total distance
-      }
-      cout << "Total: " << sum << '\n';
-
-   std::sort(distances.begin(), distances.end());
-   smallest = distances[0];
-   greatest = distances[distances.size()-1];
-   mean = sum/distances.size();
-   cout << "Smallest: " << smallest
-        << "\nLargest: " << greatest
-        << "\nMean distance: " << mean
-        << '\n';
+    int answer;
+    string reply;
+    vector<int>numbers;
+    for(int i=1; i<=100; ++i)
+        numbers.push_back(i);
+    int n = numbers.size();
+    int lo = 0;
+    int hi = n-1;
+    cout << "Think a number between one and hundred!" << '\n';
+    while(lo<=hi)
+    {
+        int mi = lo + (hi-lo)/2;
+        cout << "Is the number you are thinking of less than or equal "
+             << numbers[mi] << "? yes or no?" << '\n';
+        cin >> reply;
+        if(reply=="no")
+            lo = mi+1;
+        else
+            hi = mi-1;
+    }
+    answer = lo+1;
+    cout << "The number you thought is "
+         << answer << "!" << '\n';
 
 }

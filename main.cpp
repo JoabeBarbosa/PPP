e0776df990235664916599e58c686a8131a5a804
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-06-04 22:29:08 -0300

    Capitulo_4_Exercicio_10

--- main.cpp
+++ main.cpp
@@ -7,40 +7,53 @@ using namespace std;
 
 int main()
 {
-   int number;
-   int square_position = 1;
-   int grains_add = 1;
-   int total_grains = 1;
-   cout << "Enter a number: ";
-   cin >> number;
-   while(total_grains<number)
-       if(square_position==1)
-       {
-          cout << "Square position (" << square_position
-            << ") Grains add (" << grains_add
-            << ") Total grains (" << total_grains << ")"
-            << '\n';
-            ++square_position;
-       }
-       else if(square_position==2)
-       {
-          grains_add = 2;
-          total_grains = 3;
-          cout << "Square position (" << square_position
-               << ") Grains add (" << grains_add
-               << ") Total grains (" << total_grains << ")"
-               << '\n';
-            ++square_position;
-       }
-       else
-       {
-          grains_add *= 2;
-          total_grains += grains_add;
-          cout << "Square position (" << square_position
-               << ") Grains add (" << grains_add
-               << ") Total grains (" << total_grains << ")"
-               << '\n';
-           ++square_position;
-       }
+    cout << "Enter values r(rocket), p(paper) or s(scissors).\n";
+    vector<char>machine;
+    for(char random; cin>>random;)
+    {
+        if(random=='r'||random=='p'||random=='s')
+        machine.push_back(random);
+        else
+        break;
 
+    }
+    for(int i=0; i<machine.size(); ++i)
+    {
+        cout << "Let's play. Choose r, p or s.\n";
+        char choice;
+        cin >> choice;
+        switch(machine[i]){
+        case 'r':
+        {
+        if(choice=='r')
+        cout << "Draw. RxR\n";
+        else if(choice=='p')
+        cout << "Win. RxP\n";
+        else
+        cout << "Lose. RxS\n";
+        }
+        break;
+        case 'p':
+        {
+        if(choice=='r')
+        cout << "Lose. PxR\n";
+        else if(choice=='p')
+        cout << "Draw. PxP\n";
+        else
+        cout << "Win. PxS\n";
+        }
+        break;
+        case 's':
+        {
+        if(choice=='r')
+        cout << "Win. SxR\n";
+        else if(choice=='p')
+        cout << "Lose. SxP\n";
+        else
+        cout << "Draw. SxS\n";
+        }
+        break;}
+
+    }
 }
+

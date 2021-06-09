cc0434b8bd66e7ca5e4ab16d9bfdfc8854d9c100
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-06-09 00:52:28 -0300

    Capitulo_4_Exercicio_13

--- main.cpp
+++ main.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-
+#include <algorithm>
 
 using namespace std;
 
-int check(int x)
-{
-    if(x==1)
-    {
-        return 0;
-    }
-    else
-    {
-       for(int i=2; i<x; ++i)
-          {
-             if(x%i==0)
-              {
-               return 0;
-
-              }
-             else
-               { } //Do nothing.
-          }
-    }
-
-    return x;
-
-}
 int main()
 {
-    cout << "Enter Max.\n";
-    int Max;
-    cin >> Max;
     vector<int>primes;
-    for(int i=1; i<=Max; ++i)
-    if(check(i)!=0)
-    primes.push_back(i);
-
+    for(int i=1; i<=100; i++)
+        primes.push_back(i);
     for(int x:primes)
-    cout << x << " ";
+        cout << x << " ";
+    primes.erase(std::remove(primes.begin(), primes.end(), 1), primes.end());
+    for(int p=0; p<=10; ++p)
+    {
+        int j = primes[p]*2;
+        for(int i=j; i<=100; i+=primes[p])
+          primes.erase(std::remove(primes.begin(), primes.end(), i), primes.end());
+    }
+    cout << '\n' << '\n';
+    for(int x:primes)
+        cout << x << " ";
 }
-
-

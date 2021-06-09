19a76769e6630cfb13fcc28fe39b46671dda714d
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-06-09 18:05:28 -0300

    Capitulo_4_Exercicio_15

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter Max\n";
-    int Max;
-    cin >> Max;
+    cout << "Enter value n (n first primes)\n";
+    int n;
+    cin >> n;
     vector<int>primes;
-    for(int i=1; i<=Max; i++)
+    for(int i=1; i<=n*n; i++)
         primes.push_back(i);
-    for(int x:primes)
-        cout << x << " ";
     primes.erase(std::remove(primes.begin(), primes.end(), 1), primes.end());
-    for(int p=0; p<=sqrt(Max); ++p)
+    for(int p=0; p<=n; ++p)
     {
         int j = primes[p]*2;
-        for(int i=j; i<=Max; i+=primes[p])
+        for(int i=j; i<=n*n; i+=primes[p])
             primes.erase(std::remove(primes.begin(), primes.end(), i), primes.end());
     }
-    cout << '\n' << '\n';
-    for(int x:primes)
-        cout << x << " ";
+    cout << '\n' << '\n' << "First " << n << " primes: \n";
+    for(int x=0; x<n; ++x)
+    {
+        cout << primes[x] << " ";
+    }
 }

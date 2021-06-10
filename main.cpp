5f17b3ace4b6e2bdd72245c72e75c4515de6577a
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-06-10 01:31:21 -0300

    Capitulo_4_Exercicio_16

--- main.cpp
+++ main.cpp
@@ -7,22 +7,32 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter value n (n first primes)\n";
-    int n;
-    cin >> n;
-    vector<int>primes;
-    for(int i=1; i<=n*n; i++)
-        primes.push_back(i);
-    primes.erase(std::remove(primes.begin(), primes.end(), 1), primes.end());
-    for(int p=0; p<=n; ++p)
+    cout << "Enter a set of positive integers.\n";
+    vector<int>numbers;
+    for(int i; cin>>i;)
+        numbers.push_back(i);
+    std::sort(numbers.begin(), numbers.end());
+    for(int x:numbers)
+        cout << x << " ";
+    int score_previous = 0;
+    int score_later = 0;
+    int mode;
+    for(int p=0; p<numbers.size(); ++p)
     {
-        int j = primes[p]*2;
-        for(int i=j; i<=n*n; i+=primes[p])
-            primes.erase(std::remove(primes.begin(), primes.end(), i), primes.end());
+        int position = numbers[p];
+        for(int i=0; i<numbers.size(); ++i)
+        {
+            if(numbers[i]==position)
+                ++score_later;
+        }
+        if(score_later>score_previous)
+        {
+            mode = numbers[p];
+            score_previous=score_later;
+            score_later = 0;
+        }
+        else
+            score_later = 0;
     }
-    cout << '\n' << '\n' << "First " << n << " primes: \n";
-    for(int x=0; x<n; ++x)
-    {
-        cout << primes[x] << " ";
-    }
+    cout << "\nMode is " << mode;
 }

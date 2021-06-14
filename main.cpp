ba6cadafda4e2435cd87f85a3afbe37f749b7595
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-06-14 16:26:40 -0300

    Capitulo_4_Exercicio_17

--- main.cpp
+++ main.cpp
@@ -7,32 +7,44 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter a set of positive integers.\n";
-    vector<int>numbers;
-    for(int i; cin>>i;)
-        numbers.push_back(i);
-    std::sort(numbers.begin(), numbers.end());
-    for(int x:numbers)
-        cout << x << " ";
+    cout << "Enter names: \n";
+    vector<string>names;
+    for(string g:names)
+        cout << g << " ";
+    string para = "0";
+    for(string name; cin>>name;)
+    {
+        if(name!=para)
+        names.push_back(name);
+        else
+        break;
+    }
+    string Max, min, mode;
+    std::sort(names.begin(), names.end());
+    for(string h:names)
+        cout << h << " ";
+    min = names[0];
+    Max = names[names.size()-1];
     int score_previous = 0;
     int score_later = 0;
-    int mode;
-    for(int p=0; p<numbers.size(); ++p)
-    {
-        int position = numbers[p];
-        for(int i=0; i<numbers.size(); ++i)
+    for(int p=0; p<names.size(); ++p)
+     {
+        string position = names[p];
+        for(int i=0; i<names.size(); ++i)
         {
-            if(numbers[i]==position)
-                ++score_later;
+            if(names[i]==position)
+            ++score_later;
         }
         if(score_later>score_previous)
         {
-            mode = numbers[p];
-            score_previous=score_later;
-            score_later = 0;
+        mode = names[p];
+        score_previous=score_later;
+        score_later = 0;
         }
         else
-            score_later = 0;
-    }
-    cout << "\nMode is " << mode;
+        score_later = 0;
+     }
+    cout << "\nMax " << Max
+         << "\nmin " << min
+         << "\nmode " << mode;
 }

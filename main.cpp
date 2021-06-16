d655b2a548f3ab31e5a96a147c40ad0b79aa3c7f
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2021-06-15 22:45:01 -0300

    Capitulo_4_Exercicio_19

--- main.cpp
+++ main.cpp
@@ -7,29 +7,30 @@ using namespace std;
 
 int main()
 {
-    double a, b, c;
-    double x_1, x_2;
-    double delta;
-    cout << "Enter a quadratic equation (ax^2 + bx + c = 0). Inputs a, b, and c: ";
-    cin >> a >> b >> c;
-    cout << '\n' << a << "x^2 + " << b << "x" << " + " << c << " = 0" << '\n';
-    delta = b*b-4*a*c;
-    cout << "Delta = " << delta;
-    if(delta<0)
+   vector<string>names;
+   vector<int>scores;
+   cout << "Enter a name and score: ";
+   string name_later;
+   int score_later;
+   while(cin>>name_later>>score_later)
+   {
+        for(int i=0; i<names.size(); ++i)
         {
-           cout << "\nDelta < 0, have no real roots. Try again.";
-           return 0;
+            if(name_later==names[i])
+            {
+                cout << "\nError: name entered twice.";
+                return 0;
+            }
         }
-    else if(delta>0)
-        {
-            x_1= (-b+sqrt(delta))/2*a;
-            x_2= (-b-sqrt(delta))/2*a;
-            cout << "\nx_1 = " << x_1
-                 << "\nx_2 = " << x_2;
-        }
-    else
-        {
-            x_1= (-b+sqrt(0))/2*a;
-            cout << "x_1 = x_2 = " << x_1;
-        }
+        if(name_later=="NoName" && score_later==0)
+            break;
+        else
+            {
+                names.push_back(name_later);
+                scores.push_back(score_later);
+            }
+
+   }
+   for(int p=0; p<names.size(); ++p)
+        cout << '\n' << names[p] << " " << scores[p] << '\n';
 }

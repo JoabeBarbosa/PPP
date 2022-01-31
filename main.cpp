ec2bfcb1230d98ac7c3eb4589b8ef6716c2a7f3c
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2022-01-31 17:57:42 -0300

    Capitulo_8_Exercicio_6

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
 
-void rev_vec1(std::vector<int>vec1, std::vector<int>& v)
+using std::string;
+
+void rev_vec1(std::vector<string>vec1, std::vector<string>& v)
 {
     for(int i=vec1.size()-1; i>=0; --i)
         v.push_back(vec1[i]);
 }
 
-void rev_vec2(std::vector<int>& v)
+void rev_vec2(std::vector<string>& v)
 {
     for(int i=0, j=v.size()-1; i<j; ++i, --j)
         std::swap(v[i], v[j]);
@@ -15,25 +17,25 @@ void rev_vec2(std::vector<int>& v)
 
 int main()
 {
-    std::vector<int>test = {1, 2, 3, 4 , 5, 9, 0,};
-    std::vector<int>test2;
+    std::vector<string>test = {"1", "2", "3", "4" , "5", "9", "0"};
+    std::vector<string>test2;
     std::cout << "Original vector: ";
-    for(const int& i:test)
+    for(const string& i:test)
         std::cout << i << " ";
     std::cout << std::endl;
     rev_vec1(test, test2);
     std::cout << "rev_vec1: ";
-    for(const int& i:test2)
+    for(const string& i:test2)
         std::cout << i << " ";
     std::cout << std::endl;
     std::cout << "Original vector: ";
-    for(const int& i:test)
+    for(const string& i:test)
         std::cout << i << " ";
     std::cout << std::endl;
     std::cout << "rev_vec2: " << std::endl;
     rev_vec2(test);
     std::cout << "Original vector: ";
-    for(const int& i:test)
+    for(const string& i:test)
         std::cout << i << " ";
 
 }

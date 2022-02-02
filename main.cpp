0358296aa71fcc3abc5df80e7338c9e029ca84aa
JoabeBarbosa <joaberibeirobarbosa@gmail.com>
2022-02-02 02:32:29 -0300

    Capitulo_8_Exercicio_11

--- main.cpp
+++ main.cpp
@@ -2,33 +2,54 @@
 #include <vector>
 
 namespace exc{
-    std::vector<double>price = {2, 4, 6 , 8, 10};
-    std::vector<double>weight = {40, 60, 80, 100, 120};
-    double pw(const std::vector<double>& d, const std::vector<double>& v);
+    struct data_vec{
+        double mean;
+        double median;
+        double largest;
+        double smallest;
+    };
+    double mean;
+    double median;
+    double largest;
+    double smallest;
 }
 
-void sum_all()
+void maxv(const std::vector<double>& v)
 {
-    double d = 0;
-    if(exc::price.size()==exc::weight.size())
-        for(int i=0; i<exc::price.size(); ++i){
-            d += exc::pw(exc::price, exc::weight);
-            std::cout << "Sum of price[i]*weight[i] = " << d << std::endl;
-        }
-    else
-        throw(exc::price.size());
+    exc::mean = v[0];
+    exc::largest = v[0];
+    exc::smallest = v[0];
+    for(const double& d:v){
+        exc::mean += d;
+        if(d>exc::largest)
+            exc::largest = d;
+        if(d<exc::smallest)
+            exc::smallest = d;
+    }
+    exc::mean /= v.size();
+    exc::median = v[v.size()/2];
 }
 
-double exc::pw(const std::vector<double>& d, const std::vector<double>& v)
+exc::data_vec max_struct(const std::vector<double>& v)
 {
-    static int i = 0;
-    double j = d[i]*v[i];
-    ++i;
-    std::cout << "Multiplication of price[i]*weight[i] = " << j << '\n';
-    return j;
+    double mean = 0;
+    double largest = v[0];
+    double smallest = v[0];
+    for(const double& d:v){
+        mean += d;
+        if(d>largest)
+            largest = d;
+        if(d<smallest)
+            smallest = d;
+    }
+    mean /= v.size();
+    double median = v[v.size()/2];
+    return exc::data_vec{mean, median, largest, smallest};
 }
 
 int main()
 {
-    sum_all();
+    std::vector<double>v = {1,2,3,4,5,6,7,8,9,12,23,1234,345.65,76554,7852345.6};
+    exc::data_vec data = max_struct(v);
+    std::cout << "mean = " << data.mean << " median = " << data.median << " largest = " << data.largest << " smallest = " << data.smallest << std::endl;
 }
